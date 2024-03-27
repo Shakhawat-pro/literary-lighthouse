9c122fbd28ef1b8101df3a83725543e5e31bd9c6
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-03-27 06:51:32 +0600

    hero done

--- src/components/SoloBook.jsx
+++ src/components/SoloBook.jsx
@@ -5,15 +5,21 @@ const SoloBook = ({ book }) => {
     const { image, id, name, author, rating, category, tag1, tag2 } = book
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure className="px-10 pt-10">
-                    <img src={image} alt="Shoes" className="rounded-xl" />
+            <div className="card w-96 h-full bg-base-100 shadow-xl p-10 ">
+                <figure className="bg-base-200 p-10 rounded-xl">
+                    <img src={image} alt="Shoes" className="rounded-lg h-[280px]" />
                 </figure>
-                <div className="card-body items-center text-             center">
+                <div className="">
+                    h1
                     <h2 className="card-title">{name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <p>{author}</p>
+                    <hr />
+                    <div className='flex justify-between'>
+                        <h2>{category}</h2>
+                        <h2 className='flex gap-1'>{rating} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                        </svg>
+                        </h2>
                     </div>
                 </div>
             </div>
